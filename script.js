8b5a749182dffd1f74c2d2d9fcf1225a6821fcdb
MideJoy Akinleye <joyintech50@gmail.com>
2025-03-04 16:00:53 +0100

    final updates

--- script.js
+++ script.js
@@ -3,39 +3,47 @@ const convertButton = document.getElementById("convert-btn");
 const resultBox = document.getElementById("resultBox")
 const output = document.getElementById("output");
 
-const checkNumber = () => {
-switch (true) {
-    case (userInput.value == ""):
-        output.innerHTML = "Please enter a valid number"
-        break;
-    case (userInput.value <= 1):
-        output.innerHTML = "Please enter a number greater than or equal to 1";
-        break;
-    case (userInput.value >= 4000):
-        output.innerHTML = "Please enter a number less than or equal to 3999";
-        break;
-    default:
-        output.innerHTML = "Please enter a valid number"
-}
-}
-
+const romanNumerals = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" }
+]
 
 const convertNumber = () => {
-    resultBox.style.display = "block";
-    checkNumber()
+    resultBox.style.display = "grid";
+    let userNum = userInput.value
+    let romanResult = "";
 
+    switch (true) {
+        case (userNum == ""):
+            output.innerHTML = "Please enter a valid number"
+            break;
+        case (userNum < 1):
+            output.innerHTML = "Please enter a number greater than or equal to 1";
+            break;
+        case (userNum >= 4000):
+            output.innerHTML = "Please enter a number less than or equal to 3999";
+            break;
+        default:
+            for (let i = 0; i < romanNumerals.length; i++) {
+                while (userNum >= romanNumerals[i].value) {
+                    romanResult += romanNumerals[i].symbol;
+                    userNum -= romanNumerals[i].value;
+                }
+            }
+            output.innerHTML = romanResult;
+            break;
+    }
 }
 
 convertButton.addEventListener("click", convertNumber);
-
-
-
-// If/else statement that can be used in place of switch
-
-//     if (userInput.value == "") {
-//         output.innerHTML = "Please enter a valid number"
-//     } else if (userInput.value <= 1) {
-//         output.innerHTML = "Please enter a number greater than or equal to 1"
-//     } else if (userInput.value >= 4000) {
-//         output.innerHTML = "Please enter a number less than or equal to 3999"
-//     }
